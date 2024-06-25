bd5ce327188409077582f9582b7e194f29dda37a
Robherto <robhertomatt@gmail.com>
2024-06-25 14:01:12 +0700

    adding reddisLab Link

--- Server (LinkedInProject)/app.js
+++ Server (LinkedInProject)/app.js
@@ -7,7 +7,7 @@ const { ApolloServer } = require("@apollo/server");
 const { startStandaloneServer } = require("@apollo/server/standalone");
 const userTypeDefs = require("./schema/userSchema");
 const postTypeDefs = require("./schema/postSchema");
-const followTypeDefs = require ("./schema/followSchema")
+const followTypeDefs = require("./schema/followSchema");
 const userResolver = require("./resolver/userResolver");
 const postResolver = require("./resolver/postResolver");
 const followResolver = require("./resolver/followResolver");
@@ -15,11 +15,15 @@ const followResolver = require("./resolver/followResolver");
 const { GraphQLError } = require("graphql");
 const { verifyToken } = require("./helpers/jwt");
 
+
+
 const server = new ApolloServer({
-  typeDefs: [userTypeDefs, postTypeDefs,followTypeDefs],
-  resolvers: [userResolver, postResolver,followResolver],
+  typeDefs: [userTypeDefs, postTypeDefs, followTypeDefs],
+  resolvers: [userResolver, postResolver, followResolver],
   introspection: true,
 });
+
+
 
 // Passing an ApolloServer instance to the `startStandaloneServer` function:
 //  1. creates an Express app
@@ -27,7 +31,7 @@ const server = new ApolloServer({
 //  3. prepares your app to handle incoming requests
 
 startStandaloneServer(server, {
-  listen: { port: 4000 },
+  listen: { port: process.env.PORT || 4000 },
   context: async ({ req }) => {
     return {
       authentication: () => {
--- Server (LinkedInProject)/config/redis.js
+++ Server (LinkedInProject)/config/redis.js
@@ -3,6 +3,10 @@ const Redis = require("ioredis");
 // Create a Redis instance.
 // By default, it will connect to localhost:6379.
 // We are going to cover how to specify connection options soon.
-const redis = new Redis();
+const redis = new Redis({
+  port: 16716, // Redis port
+  host: "redis-16716.c252.ap-southeast-1-1.ec2.redns.redis-cloud.com", // Redis host
+  password: "EJr3U3NBuo0HsFSUfu3TYsC4HqJQoKtv",
+});
 
-module.exports = redis
+module.exports = redis;
